5a82e3709928b3d4614120e87aa4c74c2e154433
mdrohan <mdsajed210@gmail.com>
2023-06-11 15:59:10 +0600

    pacth user to admin

--- index.js
+++ index.js
@@ -64,6 +64,20 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     // classes part
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();

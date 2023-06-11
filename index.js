b9ccae3db15d8214fce4c4a6d42a3ab0578a548c
mdrohan <mdsajed210@gmail.com>
2023-06-11 16:01:52 +0600

    get instructor rule after patch operation

--- index.js
+++ index.js
@@ -105,6 +105,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
     // classes part
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();

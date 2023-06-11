464d2492b954ae8e76b6c76f710147e45344e951
mdrohan <mdsajed210@gmail.com>
2023-06-11 15:44:31 +0600

    delete instructor added class

--- index.js
+++ index.js
@@ -165,6 +165,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/instructorClasses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(

9c688058a403f98bcdb717360dac87590d579217
mdrohan <mdsajed210@gmail.com>
2023-06-11 16:04:51 +0600

    admin verify operation

--- index.js
+++ index.js
@@ -70,6 +70,19 @@ async function run() {
       res.send({ token });
     });
 
+    // verifyAdmin part
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
+
     //  users part
     app.get("/users", verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();

c7b537c1ad04dd33f1f968aaaa13e6213d66625d
mdrohan <mdsajed210@gmail.com>
2023-06-11 15:41:24 +0600

    get selected class

--- index.js
+++ index.js
@@ -119,6 +119,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/selectedclass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedclassCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
